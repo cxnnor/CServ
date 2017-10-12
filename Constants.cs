2177f5f9347c470ba8f758599cd0e486917d4a52
Con <con@UNKNOWN.local>
2017-10-12 15:59:23 +0100

    Base
    Micro HTTP Server
    Displaying HTML
    Constants added

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SharpSRV
+{
+    public class Constants
+    {
+        public Constants()
+        {
+        }
+
+		public const string name = "SharpSRV";
+		public const float version = 1.5f;
+    }
+}
---
+++ HTTPserver.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace SharpSRV
+{
+	public class HTTPserver
+	{
+		private readonly HttpListener lsnr = new HttpListener();
+		private readonly Func<HttpListenerRequest, string> rspn;
+
+		public HTTPserver(IReadOnlyCollection<string> prefixes, Func<HttpListenerRequest, string> method)
+		{
+			if (!HttpListener.IsSupported)
+			{
+				throw new NotSupportedException("OS not supported");
+			}
+
+			if (prefixes == null || prefixes.Count == 0)
+			{
+				throw new ArgumentException("uri prefix required");
+			}
+
+			if (method == null)
+			{
+				throw new ArgumentException("responder method required");
+			}
+
+			foreach (var s in prefixes)
+			{
+				lsnr.Prefixes.Add(s);
+			}
+
+			rspn = method;
+			lsnr.Start();
+		}
+
+		public HTTPserver()
+		{
+
+		}
+
+		public HTTPserver(Func<HttpListenerRequest, string> method, params string[] prefixes)
+		   : this(prefixes, method)
+		{
+		}
+
+		public void Run()
+		{
+			ThreadPool.QueueUserWorkItem(o =>
+			{
+				Console.WriteLine("Webserver running...");
+				try
+				{
+					while (lsnr.IsListening)
+					{
+						ThreadPool.QueueUserWorkItem(c =>
+						{
+							var ctx = c as HttpListenerContext;
+							try
+							{
+								if (ctx == null)
+								{
+									return;
+								}
+
+                                var rstr = rspn(ctx.Request);
+								var buf = Encoding.UTF8.GetBytes(rstr);
+								ctx.Response.ContentLength64 = buf.Length;
+								ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+							}
+							catch
+							{
+								// ignore
+							}
+							finally
+							{
+
+								if (ctx != null)
+								{
+									ctx.Response.OutputStream.Close();
+								}
+							}
+						}, lsnr.GetContext());
+					}
+				}
+				catch (Exception ex)
+				{
+					// ignore
+				}
+			});
+		}
+
+		public void Stop()
+		{
+			lsnr.Stop();
+			lsnr.Close();
+		}
+	}
+
+	internal class Program
+	{
+		public static string SendResponse(HttpListenerRequest request)
+		{
+            return string.Format("<HTML><BODY>{1} is successfully running version {2} <br>The current date and time is {0}</BODY></HTML>", DateTime.Now, Constants.name, Constants.version);
+		}
+
+		public static void Main(string[] args)
+		{
+			HTTPserver ws = new HTTPserver(SendResponse, "http://localhost:8080/test/");
+
+			ws.Run();
+            Console.WriteLine("Server started on localhost:8080. Press a key to quit.");
+			Console.ReadKey();
+			ws.Stop();
+
+		}
+	}
+}
